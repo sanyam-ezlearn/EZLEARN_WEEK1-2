882113d2570c4a85b70da35c55346cbf2ee606ae
sanyam-ezlearn <sanyamj@ezlearn.in>
2025-07-22 10:26:51 +0530

    done ques 15

---
+++ pq15.py
@@ -0,0 +1,11 @@
+'''Detect Capital'''
+def detectCapitalUse(word):
+    if word.isupper():
+        return True
+    elif word.islower():
+        return True
+    elif word.istitle():
+        return True
+    else:
+        return False
+print(detectCapitalUse("USA"))
